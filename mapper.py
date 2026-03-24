183b6236245152f80747e9ad2833c2266ccad7aa
emili0p <emiliioizq23@gmail.com>
2026-03-23 22:52:31 -0600

    commit inicial

---
+++ mapper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+counts = {}
+
+for line in sys.stdin:
+    # regex para limpiar texto, quita minuscalas y puntuaciónes
+    words = re.findall(r"\b\w+\b", line.lower())
+    for w in words:
+        counts[w] = counts.get(w, 0) + 1
+
+# evita conteos parciales
+for w, c in counts.items():
+    print(f"{w}\t{c}")
---
+++ reducer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+import signal
+
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+palabra_actual = None
+contador = 0
+
+for line in sys.stdin:
+    palabra, cuenta = line.strip().split("\t")
+    cuenta = int(cuenta)
+
+    if palabra == palabra_actual:
+        contador += cuenta  # ✅ aquí estaba el error
+    else:
+        if palabra_actual is not None:
+            print(f"{palabra_actual}\t{contador}")
+        palabra_actual = palabra
+        contador = cuenta
+
+# imprimir último
+if palabra_actual is not None:
+    print(f"{palabra_actual}\t{contador}")
